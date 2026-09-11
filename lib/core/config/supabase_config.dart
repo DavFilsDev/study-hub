74151fc9015d28068b42c50e3e183ddff7a82ed8
DavFilsDev <miharisoadavidfils@gmail.com>
2026-09-11 14:14:16 +0300

    chore(supabase): add database schema, RLS policies and seed data for courses and resources

---
+++ lib/core/config/supabase_config.dart
@@ -0,0 +1,7 @@
+class SupabaseConfig {
+  static const String url = 'https://rnvexeiutyszdvflwuwt.supabase.co';
+  static const String anonKey =
+      'sb_publishable_3SEOv22ni9wlaeeMXAQkEg_uO8QR63e';
+
+  static const String restUrl = '$url/rest/v1';
+}
